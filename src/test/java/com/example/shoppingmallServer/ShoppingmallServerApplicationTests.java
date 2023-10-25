9e698f972f3bfc65d022ece60b48fad63219f505
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 <wngns0812@gmail.com>
2023-10-25 14:18:50 +0900

    chore: docker image push 에러 수정

--- src/test/java/com/example/shoppingmallServer/ShoppingmallServerApplicationTests.java
+++ src/test/java/com/example/shoppingmallServer/ShoppingmallServerApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.example.shoppingmallServer;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-////@SpringBootTest
-//class ShoppingmallServerApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.example.shoppingmallServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
