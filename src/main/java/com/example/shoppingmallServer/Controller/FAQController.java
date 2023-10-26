fd2d01d23638141f07a666089022c44dfac4a8af
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 <wngns0812@gmail.com>
2023-10-27 02:24:35 +0900

    ci: docker 중지 명령어 추가

--- src/main/java/com/example/shoppingmallServer/Controller/FAQController.java
+++ src/main/java/com/example/shoppingmallServer/Controller/FAQController.java
@@ -7,6 +7,9 @@ import com.example.shoppingmallServer.Response.FAQResponse;
 import com.example.shoppingmallServer.Service.FAQService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,10 +19,12 @@ import java.util.List;
 @RequestMapping("/FAQ")
 public class FAQController {
     private final FAQService faqService;
-
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     @PostMapping("/insert")
-    public ResponseEntity<String> insert(@RequestHeader("Authorization") String accessToken, @RequestBody FAQDto faqDto) {
-        return faqService.insert(faqDto, accessToken);
+    public void insert(@RequestBody FAQDto faqDto) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println(userDetails);
+//        return faqService.insert(faqDto, accessToken);
     }
 
     @GetMapping("/findAll")
--- src/main/java/com/example/shoppingmallServer/Controller/MemberController.java
+++ src/main/java/com/example/shoppingmallServer/Controller/MemberController.java
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
