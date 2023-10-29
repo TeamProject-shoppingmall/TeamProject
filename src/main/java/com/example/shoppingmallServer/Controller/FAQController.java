33217991e9f138129c9b9cdc0f381bed905357ec
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 <wngns0812@gmail.com>
2023-10-29 16:53:33 +0900

    ci: docker run 이름 수정

--- src/main/java/com/example/shoppingmallServer/Controller/FAQController.java
+++ src/main/java/com/example/shoppingmallServer/Controller/FAQController.java
@@ -7,9 +7,6 @@ import com.example.shoppingmallServer.Response.FAQResponse;
 import com.example.shoppingmallServer.Service.FAQService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,12 +16,9 @@ import java.util.List;
 @RequestMapping("/FAQ")
 public class FAQController {
     private final FAQService faqService;
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     @PostMapping("/insert")
-    public void insert(@RequestBody FAQDto faqDto) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        System.out.println(userDetails);
-//        return faqService.insert(faqDto, accessToken);
+    public ResponseEntity<String> insert(@RequestHeader("Authorization") String accessToken, @RequestBody FAQDto faqDto) {
+        return faqService.insert(faqDto, accessToken);
     }
 
     @GetMapping("/findAll")
